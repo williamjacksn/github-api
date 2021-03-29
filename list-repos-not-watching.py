555a4b25235b6cabf6e13511767e49400cb138bc
William Jackson <william@subtlecoolness.com>
2021-03-29 20:35:18 +0000

    Update and rename repos-not-watching script

---
+++ list-repos-not-watching.py
@@ -0,0 +1,19 @@
+import github
+
+
+def main():
+    client = github.GitHubClient()
+    for repository in client.get_repositories():
+        subscribed = False
+        subscribers_url = repository.get('subscribers_url')
+        subscribers = client.call(subscribers_url)
+        for subscriber in subscribers:
+            if subscriber.get('login') == client.username:
+                subscribed = True
+                break
+        if not subscribed:
+            print(repository.get('full_name'))
+
+
+if __name__ == '__main__':
+    main()
