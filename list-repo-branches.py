ef8d5cbf1ddc349df043bba6cc0f37f2a0ffab92
William Jackson <565174+williamjacksn@users.noreply.github.com>
2026-06-05 08:40:01 -0500

    Use indexing instead of `get()`

--- list-repo-branches.py
+++ list-repo-branches.py
@@ -4,7 +4,7 @@ import github
 def main() -> None:
     client = github.GitHubClient()
     for repository in client.get_repositories():
-        repo_full_name = repository.get("full_name")
+        repo_full_name = repository["full_name"]
         for branch in client.get_branches(repo_full_name):
             branch_name = branch.get("name")
             if branch_name not in ["main", "master"]:
--- list-repo-secrets.py
+++ list-repo-secrets.py
@@ -4,7 +4,7 @@ import github
 def main() -> None:
     client = github.GitHubClient()
     for repository in client.get_repositories():
-        repo_full_name = repository.get("full_name")
+        repo_full_name = repository["full_name"]
         for secret in client.get_secrets(repo_full_name):
             secret_name = secret.get("name")
             print(f"{repo_full_name}/{secret_name}")
